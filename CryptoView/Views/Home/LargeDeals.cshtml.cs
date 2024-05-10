b73926ba23583adb4592fd239cc41c21fc444430
chikanerick <115743340+chikanerick@users.noreply.github.com>
2024-05-10 22:16:24 +0300

    Добавьте файлы проекта.

---
+++ CryptoView/Views/Home/LargeDeals.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CryptoView.Views.Home
+{
+    public class LargeDealsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CryptoView/Views/Home/Market.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CryptoView.Views.Home
+{
+    public class MarketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
