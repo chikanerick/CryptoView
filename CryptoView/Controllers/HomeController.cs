a604692ea716aaac763d81d52833b1196237bf10
chikanerick <115743340+chikanerick@users.noreply.github.com>
2024-05-13 19:10:40 +0300

    added news

--- CryptoView/Controllers/HomeController.cs
+++ CryptoView/Controllers/HomeController.cs
@@ -52,6 +52,11 @@ namespace CryptoView.Controllers
             return View();
         }
 
+        public IActionResult News()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
